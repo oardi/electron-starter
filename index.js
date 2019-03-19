9af438e1b206cf08d3fe27aae94af61237d1aed5
Ardian Shala <ardian.sh@gmx.de>
2019-03-19 11:10:27 +0100

    updated to electron 4

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
-const {app, BrowserWindow} = require("electron");
+const { app, BrowserWindow } = require("electron");
 
 let win = null;
 
 app.on("ready", function () {
-    win = new BrowserWindow({
-        width: 1200,
-        height: 720,
-        //autoHideMenuBar: true
-    });
 
-    //load local index.html file
-    win.loadURL('file://' + __dirname + '/src/index.html');
+	win = new BrowserWindow({
+		width: 1200,
+		height: 720,
+		//autoHideMenuBar: true
+	});
 
-    //open dev tools with app start
-    win.webContents.openDevTools();
+	//load local index.html file
+	win.loadURL('file://' + __dirname + '/src/index.html');
 
-    //clear win on closed
-    win.on('closed', function () {
-        win = null;
-    });
+	//open dev tools with app start
+	win.webContents.openDevTools();
+
+	//clear win on closed
+	win.on('closed', function () {
+		win = null;
+	});
+
 });
 
 //createWindow on activate
 app.on('activate', function () {
-    if (win == null) {
-        createWindow();
-    }
-})
+	if (win == null) {
+		createWindow();
+	}
+});
 
 //quit the app from the process
 app.on('window-all-closed', function () {
-    if (process.platform != 'darwin') {
-        app.quit();
-    }
+	if (process.platform != 'darwin') {
+		app.quit();
+	}
 });
